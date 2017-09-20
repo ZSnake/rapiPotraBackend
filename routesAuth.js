977c3068bec75647b86da656c125839728a570ba
fmaldonados <felixmaldonado@unitec.edu>
2017-09-19 20:44:02 -0600

    hice algunos cambios en la auth

--- routesAuth.js
+++ routesAuth.js
@@ -2,5 +2,6 @@ var authController = require('./controllers/authController');
 
 exports.endpoints = [
 	{method: 'POST', path: '/login', config: authController.login},
+	{method: 'POST', path: '/loginLocal', config: authController.loginLocal},
 	{method: 'GET', path: '/logout', config: authController.logout}
 ];
